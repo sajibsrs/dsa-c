16e90bf6717dfeefe7883ee3f74d059699d50c05
Sajidur Rahman <sajibsrs@gmail.com>
2025-09-30 23:17:33 +0600

    added memory management helpers

--- src/helper.c
+++ src/helper.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include "helper.h"
+
+void *_malloc(size_t _size) {
+    void *ptr = malloc(_size);
+    if (!ptr) exit(11);
+    return ptr;
+}
+
+void *_calloc(size_t _count, size_t _size) {
+    void *ptr = calloc(_count, _size);
+    if (!ptr) exit(11);
+    return ptr;
+}
+
+void _free(void **_ptr) {
+    if (_ptr && *_ptr) {
+        free(*_ptr);
+        *_ptr = NULL;
+    }
+}
 
 void print_byte(unsigned char n) {
     unsigned char mask = 1 << 7; // 1 is 0011 0001
--- src/helper.h
+++ src/helper.h
@@ -1,6 +1,14 @@
 #ifndef HELPER_H
 #define HELPER_H
 
+#include <stdlib.h>
+
+#define __free(_ptr) _free((void **)&(_ptr))
+
+void *_malloc(size_t _size);
+void *_calloc(size_t _count, size_t _size);
+void  _free(void **_ptr);
+
 /**
  * @brief Prints bits in a byte.
  * @param n Byte to print.
