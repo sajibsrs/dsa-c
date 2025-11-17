b8f1d1d2e4ee828ee32b84f682571b2402fabda7
Sajidur Rahman <sajibsrs@gmail.com>
2025-11-17 14:09:53 +0600

    added simpler linked list

--- src/dtst/list.c
+++ src/dtst/list.c
@@ -1,114 +1,61 @@
-#include <stdlib.h>
+#include "../helper.h"
 #include "dtst.h"
 
-/**************************************************
- * Singly linked list
- **************************************************/
+static listn_t *node_new(int data) {
+    listn_t *node = _malloc(sizeof(listn_t));
+    node->data = data;
+    node->next = NULL;
 
-void slist_init(slist_t *list, void (*destroy)(void *data)) {
-    list->head = list->tail = NULL;
-    list->size = 0;
-    list->destroy = destroy;
+    return node;
 }
 
-int slist_head_ins(slist_t *list, void *data) {
-    slnode_t *new = malloc(sizeof(slnode_t));
-    if (new == NULL) return 0;
+list_t *list_new() {
+    list_t *list = _malloc(sizeof(list_t));
+    list->size = 0;
+    list->head = NULL;
 
-    new->data = data;
-    new->next = list->head;
-    list->head = new;
+    return list;
+}
 
-    if (list->tail == NULL) list->tail = new;
+void list_ins(list_t *list, int data) {
+    listn_t *node = node_new(data);
+    node->next = list->head;
+    list->head = node;
+
     list->size++;
-    return 1;
 }
 
-int slist_tail_rem(slist_t *list, void **data) {
-    if (list->head == NULL) return 0;
+void list_rem(list_t *list, int data) {
+    if (list->head == NULL) return;
 
-    if (data != NULL) *data = list->tail->data;
+    listn_t *node = list->head;
+    listn_t *prev = NULL;
 
-    if (list->head == list->tail) {
-        free(list->tail);
-        list->head = list->tail = NULL;
-    } else {
-        slnode_t *curr = list->head;
-        while (curr->next != list->tail) curr = curr->next;
-
-        free(list->tail);
-        curr->next = NULL;
-        list->tail = curr;
+    while (node) {
+        if (node->data == data) {
+            if (prev == NULL) {
+                list->head = node->next;
+            } else {
+                prev->next = node->next;
+            }
+            _free(node);
+            list->size--;
+            return;
+        }
+        prev = node;
+        node = node->next;
     }
-    list->size--;
-    return 1;
 }
 
-void slist_rem(slist_t *list) {
-    slnode_t *curr = list->head;
+void list_free(list_t *list) {
+    if (list == NULL) return;
 
-    while (curr != NULL) {
-        slnode_t *next = curr->next;
-        if (list->destroy != NULL) list->destroy(curr->data);
-        free(curr);
-        curr = next;
+    listn_t *node = list->head;
+
+    while (node) {
+        listn_t *next = node->next;
+        _free(node);
+        node = next;
     }
-    list->head = list->tail = NULL;
-    list->size = 0;
-}
-
-/**************************************************
- * Doubly linked list
- **************************************************/
-
-void dlist_init(dlist_t *list, void (*destroy)(void *data)) {
-    list->head = list->tail = NULL;
-    list->size = 0;
-    list->destroy = destroy;
-}
-
-int dlist_head_ins(dlist_t *list, void *data) {
-    dlnode_t *new = malloc(sizeof(dlnode_t));
-    if (new == NULL) return 0;
-
-    new->data = data;
-    new->prev = NULL;
-    new->next = list->head;
-
-    if (list->head != NULL) list->head->prev = new;
-    list->head = new;
-
-    if (list->tail == NULL) list->tail = new;
-    list->size++;
-    return 1;
-}
-
-int dlist_tail_rem(dlist_t *list, void **data) {
-    if (list->tail == NULL) return 0;
-
-    dlnode_t *old = list->tail;
-    if (data != NULL) *data = old->data;
-
-    if (list->head == list->tail) {
-        list->head = list->tail = NULL;
-    } else {
-        list->tail = old->prev;
-        list->tail->next = NULL;
-    }
-    free(old);
-    list->size--;
-    return 1;
-}
-
-void dlist_rem(dlist_t *list) {
-    dlnode_t *curr = list->head;
-
-    while (curr != NULL) {
-        dlnode_t *next = curr->next;
-        if (list->destroy != NULL) list->destroy(curr->data);
-        free(curr);
-        curr = next;
-    }
-    list->head = list->tail = NULL;
-    list->size = 0;
+    _free(list);
 }
