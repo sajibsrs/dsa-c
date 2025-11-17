dd0a28c65481a520729b573ff606ff451f138d44
Sajidur Rahman <sajibsrs@gmail.com>
2025-11-17 14:09:28 +0600

    refactored

--- src/dtst/dtst.h
+++ src/dtst/dtst.h
@@ -1,64 +1,74 @@
 #ifndef DTST_H
 #define DTST_H
 
+#include <stddef.h>
+
 /**************************************************
- * - Singly linked list / Linked list
+ * - Linked list / Singly linked list
+ *
+ * It's a simple implementation of linked list.
+ * Only the head is used for insertion and deletion.
  **************************************************/
 
-typedef struct slnode {
-    void *data;
-    struct slnode *next;
-} slnode_t;
+// Linked list node
+typedef struct listn {
+    int data;
+    struct listn *next;
+} listn_t;
 
+// Linked list
 typedef struct {
-    slnode_t *head;
-    slnode_t *tail;
-    size_t size;
-    void (*destroy)(void *data);
-} slist_t;
+    int size;
+    listn_t *head;
+} list_t;
 
-void slist_init(slist_t *list, void (*destroy)(void *data));
-int slist_head_ins(slist_t *list, void *data);
-int slist_tail_rem(slist_t *list, void **data);
-void slist_rem(slist_t *list);
+/**
+ * @brief Creates new linked list.
+ * @return Linked list pointer.
+ */
+list_t *list_new();
+
+/**
+ * @brief Creates new `node` from data and inserts
+ * into the linked list.
+ * @param list Linked list pointer.
+ * @param data Node data.
+ */
+void list_ins(list_t *list, int data);
+void list_rem(list_t *list, int data);
+void list_free(list_t *list);
 
 /**************************************************
  * - Doubly linked list
  **************************************************/
 
-typedef struct dlnode {
+typedef struct dlistn {
     void *data;
-    struct dlnode *prev;
-    struct dlnode *next;
-} dlnode_t;
+    struct dlistn *prev;
+    struct dlistn *next;
+} dlistn_t;
 
-typedef struct dlist {
-    dlnode_t *head;
-    dlnode_t *tail;
-    size_t size;
-    void (*destroy)(void *data);
+typedef struct {
+    int size;
+    dlistn_t *head;
+    dlistn_t *tail;
 } dlist_t;
-
-void dlist_init(dlist_t *list, void (*destroy)(void *data));
-int dlist_head_ins(dlist_t *list, void *data);
-int dlist_tail_rem(dlist_t *list, void **data);
-void dlist_rem(dlist_t *list);
 
 /**************************************************
  * - Stack
  **************************************************/
 
 typedef struct {
-    int *stack;
+    int *arr;
     int top;
     int size;
 } stck_t;
 
-stck_t *stack_new(int size);
-void stack_push(stck_t *s, int elm);
-int stack_pop(stck_t *s);
-int stack_peek(stck_t *s);
-int stack_is_empty(stck_t *s);
+stck_t *stk_new(int size);
+int stk_push(stck_t *s, int elm);
+int stk_pop(stck_t *s);
+int stk_peek(stck_t *s);
+int stk_isempty(stck_t *s);
 
 /**************************************************
  * - Queue
@@ -73,12 +83,12 @@ typedef struct {
     int size;
 } que_t;
 
-void queue_enqueue(que_t *q, int elm);
-int queue_dequeue(que_t *q);
-int queue_peek(que_t *q);
-int queue_is_empty(que_t *q);
-int queue_size(que_t *q);
-void queue_print(que_t *q);
+void que_enqueue(que_t *q, int elm);
+int que_dequeue(que_t *q);
+int que_peek(que_t *q);
+int que_isempty(que_t *q);
+int que_size(que_t *q);
+void que_print(que_t *q);
 
 /**************************************************
  * - Trie
@@ -87,7 +97,7 @@ void queue_print(que_t *q);
 #define TRIE_SIZE 26
 
 typedef struct trie {
-    int is_end;
+    int isend;
     struct trie *child[TRIE_SIZE];
 } trie_t;
 
@@ -116,8 +126,6 @@ void agr_vdata_set(agr_t *g, int vertex, char data);
 void agr_print(const agr_t *g);
 void agr_free(agr_t *g);
 
-void agr_dfs(const agr_t *g, char start_vdata);
-void agr_bfs(const agr_t *g, char start_vdata);
 int agr_has_cycle(const agr_t *g);
 
 /* - Sparse Graph Implementation
@@ -143,8 +151,6 @@ void sgr_vdata_set(sgr_t *g, int vindex, char data);
 void sgr_print(const sgr_t *g);
 void sgr_free(sgr_t *g);
 
-void sgr_dfs(const sgr_t *g, char start_vtx);
-void sgr_bfs(const sgr_t *g, char start_vtx);
 int sgr_has_cycle(const sgr_t *g);
 
 #endif
