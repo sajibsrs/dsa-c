14607cece113c09cc8f717f5660d2cfafd13653f
Sajidur Rahman <sajibsrs@gmail.com>
2025-11-18 17:13:24 +0600

    added circular array based queue functions

--- src/dtst/queue.c
+++ src/dtst/queue.c
@@ -1,46 +1,58 @@
-#include <stdio.h>
+#include "../helper.h"
 #include "dtst.h"
 
-void queue_enqueue(que_t *q, int elm) {
-    if (q->size == QUEUE_SIZE) {
-        printf("Queue is full\n");
-        return;
+aque_t *aque_new(int capacity) {
+    aque_t *queue = _malloc(sizeof(aque_t));
+    queue->capacity = capacity;
+    queue->front = 0;
+    queue->back = -1;
+    queue->size = 0;
+    queue->array = _malloc(capacity * sizeof(int));
+
+    return queue;
+}
+
+void aque_enq(aque_t *queue, int data) {
+    if (queue->size == queue->capacity) {
+        int precap = queue->capacity;
+        queue->capacity *= 2;
+
+        int *newarr = _malloc(queue->capacity * sizeof(int));
+
+        for (int i = 0; i < queue->size; i++) {
+            newarr[i] = queue->array[(queue->front + i) % precap];
+        }
+        _free(queue->array);
+        queue->array = newarr;
+        queue->front = 0;
+        queue->back = queue->size - 1;
     }
-    q->queue[(q->front + q->size) % QUEUE_SIZE] = elm;
-    q->size++;
+    queue->back = (queue->back + 1) % queue->capacity;
+    queue->array[queue->back] = data;
+    queue->size++;
 }
 
-int queue_dequeue(que_t *q) {
-    if (queue_is_empty(q)) {
-        printf("Queue is empty\n");
-        return -1;
+int aque_deq(aque_t *queue) {
+    if (queue->size == 0) return -1;
+
+    int data = queue->array[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size--;
+
+    if (queue->size == 0) {
+        queue->front = 0;
+        queue->back = -1;
     }
-    int item = q->queue[q->front];
-    q->front = (q->front + 1) % QUEUE_SIZE;
-    q->size--;
-    return item;
+
+    return data;
 }
 
-int queue_peek(que_t *q) {
-    if (queue_is_empty(q)) {
-        printf("Queue is empty");
-        return -1;
-    }
-    return q->queue[q->front];
+int aque_peek(aque_t *queue) {
+    if (queue->size == 0) return -1;
+    return queue->array[queue->front];
 }
 
-int queue_is_empty(que_t *q) {
-    return q->size == 0;
-}
-
-int queue_size(que_t *q) {
-    return q->size;
-}
-
-void queue_print(que_t *q) {
-    printf("Queue: ");
-    for (int i = 0; i < q->size; ++i) {
-        printf("%c ", q->queue[(q->front + i) % QUEUE_SIZE]);
-    }
-    printf("\n");
+void aque_free(aque_t *queue) {
+    _free(queue->array);
+    _free(queue);
 }
