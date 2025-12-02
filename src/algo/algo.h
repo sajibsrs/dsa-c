771c41710a16e9eb4f126f3700e4acf85f8ed70d
Sajidur Rahman <sajibsrs@gmail.com>
2025-12-03 04:59:33 +0600

    updates search signatures

--- src/algo/algo.h
+++ src/algo/algo.h
@@ -1,9 +1,12 @@
 #ifndef ALGO_H
 #define ALGO_H
 
+#include "../dtst/dtst.h"
+#include "../helper.h"
+
 /**************************************************
  * - Sorting
- * -- Insertion
+ * -- Insertion sort
  **************************************************/
 
 /**
@@ -12,7 +15,31 @@
  * @param len Length of the array.
  * @return
  */
-void ins_srt(int arr[], int len);
+void insrt(int *arr, int len);
+
+/**************************************************
+ * - Searching
+ * -- Linear search
+ * -- Binary search
+ **************************************************/
+
+/**
+ * @brief Linear search algorithm.
+ * @param arr Array that need to be searched.
+ * @param size Length of the array.
+ * @param targ Target number.
+ * @return Index or -1 on failure.
+ */
+int srchlin(int *arr, int size, int targ);
+
+/**
+ * @brief Binary search algorithm.
+ * @param arr Array that need to be searched.
+ * @param size Length of the array.
+ * @param targ Target number.
+ * @return Index or -1 on failure.
+ */
+int srchbin(int *arr, int size, int targ);
 
 /**************************************************
  * - Recursions
@@ -25,7 +52,7 @@ void ins_srt(int arr[], int len);
  * @param n Total steps.
  * @return `nth` fibonacci number.
  */
-int fib_rec(int n);
+int recr_fib(int n);
 
 /**
  * @brief Get `nth` fibonacci number using tail-recursion.
@@ -34,14 +61,14 @@ int fib_rec(int n);
  * @param b Number for the next step (starting with Fib(1)).
  * @return `nth` fibonacci number.
  */
-int fib_trc(int n, int a, int b);
+int trecr_fib(int n, int a, int b);
 
 /**
  * @brief Get factorial of a number with recursion.
  * @param n Number.
  * @return Factorial.
  */
-int fact_rec(int n);
+int recr_fact(int n);
 
 /**
  * @brief Get factorial of a number with tail-recursion.
@@ -49,6 +76,14 @@ int fact_rec(int n);
  * @param a Accumulator.
  * @return Factorial.
  */
-int fact_trc(int n, int a);
+int trecr_fact(int n, int a);
+
+void agraph_dfs(const agr_t *g, char start_vdata);
+void agraph_bfs(const agr_t *g, char start_vdata);
+
+void sgraph_dfs(const sgr_t *g, char start_vtx);
+void sgraph_bfs(const sgr_t *g, char start_vtx);
+
+void dijkstra(sgr_t *g, int start_vtx, int *distances, int *predecessors);
 
 #endif
