22dd9a18eea720ba34cc6131db86c6fe96c884a7
Sajidur Rahman <sajibsrs@gmail.com>
2025-12-03 05:00:28 +0600

    updated functions

--- tests/search_test.c
+++ tests/search_test.c
@@ -1,13 +1,13 @@
 #include "../src/algo/algo.h"
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
 int arr[] = {8, 4, 5, 2, 7, 1, 6, 9, 0};
 int linarr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 int main(void) {
-    int idx = srchlin(5, arr, 10);
-    int binidx = srchbin(5, linarr, 10);
+    int idx = srchlin(arr, 10, 5);
+    int binidx = srchbin(linarr, 10, 5);
 
     printf("Search Algorithms:\n");
     printf("----------------------------------\n");
