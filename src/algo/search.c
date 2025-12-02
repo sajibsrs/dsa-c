ba01e3c9178eac77483e1a7371d0eae7f151be7d
Sajidur Rahman <sajibsrs@gmail.com>
2025-12-03 05:00:10 +0600

    fixed bug, organized code

--- src/algo/search.c
+++ src/algo/search.c
@@ -1,25 +1,22 @@
 #include "algo.h"
 
-int srchlin(int targ, int *arr, int len) {
-    for (int i; i < len; i++) {
+int srchlin(int *arr, int size, int targ) {
+    for (int i = 0; i < size; i++) {
         if (arr[i] == targ) return i;
     }
     return -1;
 }
 
-int srchbin(int targ, int *arr, int len) {
-    int high = len - 1, mid, low = 0;
+int srchbin(int *arr, int size, int targ) {
+    int left = 0;
+    int right = size - 1;
 
-    while (low <= high) {
-        mid = (high + low) / 2;
+    while (left <= right) {
+        int mid = (right + left) / 2;
 
-        if (arr[mid] == targ) {
-            return mid;
-        } else if (arr[mid] < targ) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
+        if (targ == arr[mid]) return mid;
+        if (targ > arr[mid]) left = mid + 1;
+        else right = mid - 1;
     }
     return -1;
 }
